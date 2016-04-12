85df0d77ab996e928be54ca1a57d60e60c8d1f2d
Joe Liu <joe.luee@outlook.com>
2016-04-12 17:37:04 +0800

    half complete user logup, login and logout

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,23 @@
+module SessionsHelper
+
+	# login identify user
+	def log_in(user)
+		session[:user_id] = user.id 
+	end
+
+	# return current user if exist
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# if user has logged in, return true, else return false
+	def logged_in?
+		!current_user.nil?
+	end
+
+	# logout current user
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
+	end
+end
