80c055ac8709056ddb5f23759a74c39cc18064bb
Joe Liu <joe.luee@outlook.com>
2016-04-13 17:47:40 +0800

    half finish mailer and activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	attr_accessor 	:remember_token, :activation_token, :activation_digest
+	before_save 	:downcase_email
+	before_create	:create_activation_digest
 	has_many :microposts
 	before_save { self.email = email.downcase }
 	validates :name,  presence: true, length: { maximum: 50 }
@@ -15,4 +18,33 @@ class User < ActiveRecord::Base
 														BCrypt::Engine.cost 
 		BCrypt::Password.create(string, cost: cost)
 	end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_token.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+
+	private 
+
+		def downcase_email
+			self.email = email.downcase 
+		end
+
+		def create_activation_digest
+			self.activation_token 	= User.new_token
+			self.activation_digest 	= User.digest(activation_token)
+		end
 end
