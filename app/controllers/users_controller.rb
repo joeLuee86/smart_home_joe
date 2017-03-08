5614d190024e34916a45a3ba01e7a3a6c74fe9ab
Joe Liu <joe.luee@outlook.com>
2017-03-08 22:21:16 +0800

    disable the activation of email for user register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,8 +33,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to active your account."
+      #@user.send_activation_email
+      #flash[:info] = "Please check your email to active your account."
       redirect_to root_url
     else
       render 'new'
