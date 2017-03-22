6bba854669a0c36f40310c98f0f6b2835ec175ce
joe.qcom <joe.luee@outlook.com>
2017-03-22 14:46:33 +0800

    add json for washer

--- app/controllers/washers_controller.rb
+++ app/controllers/washers_controller.rb
@@ -1,4 +1,5 @@
 class WashersController < ApplicationController
+	protect_from_forgery with: :null_session
   def new
   end
 
@@ -36,8 +37,35 @@ class WashersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /washers/1
+  # PATCH/PUT /washers/1.json
+  def update
+    @washer = Washer.find(params[:id])
+    if @washer.update_attributes(washer_params)
+      flash[:success] = "Profile updated"
+      redirect_to washers_path
+    else
+      render 'new'
+    end
+    # respond_to do |format|
+    #   if @washer.update(washer_params)
+    #     format.json { render :show, status: :ok, location: @user }
+    #   else
+    #     format.json { render json: @washer.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    Washer.find(params[:id]).destroy
+    flash[:success] = "Washer deleted"
+    redirect_to washers_path
+  end
+
   private
   	def washer_params
-      params.require(:washer).permit(:name, :channel, :start_at, :intervel)
+      params.require(:washer).permit(:name, :channel, :start_at, :intervel, :start_stop)
     end
 end
